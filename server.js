966d846db90e67663fa2f67f1f092a5b6f6ae17f
Ebenezer <ebenirene5@gmail.com>
2024-10-08 14:46:57 +0100

    added server and installed packages

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+require("dotenv").config();
+const PORT = process.env.PORT || 5001;
+
+
+
+const server = http.createServer((req, res) => {});
+
+server.listen(PORT, () => {
+  console.log(`Server started on port : ${PORT}`);
+});
